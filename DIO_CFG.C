86e6c8bfc0ee18531a16ceeaef0a9803483967dc
EL_abbas Salah Hatata <58460976+Abbassalah@users.noreply.github.com>
2020-03-03 14:59:42 +0200

    Add files via upload

---
+++ DIO_CFG.C
@@ -0,0 +1,26 @@
+/*
+ * DIO_CFG.C
+ *
+ *  Created on:  MONDAY_03/02/2020_8:16 AM
+ *      Author: ABBAS SALAH HATATA
+ */
+/***************************************#INCLUDE_LIBRARY***************************************************/
+#include "DIO.h"
+#include "DIO_CFG.h"
+#include "STD.h"
+
+/*DIO_CFG   ARRAY_OF_CFG [CHANNEL_NUM]={
+//LED0
+	    {PORT_A,PIN_0,OUTPUT,STD_HIGH,NOT_CHANGE},
+//LED1
+		{PORT_A,PIN_7,OUTPUT,STD_HIGH,NOT_CHANGE},
+//LED2
+		{PORT_C,PIN_0,OUTPUT,STD_HIGH,NOT_CHANGE},
+//SWICH0
+		{PORT_C,PIN_7,INPUT,DONT_CARE,DISPULL_UP},
+//SWICH1
+		{PORT_C,PIN_1,INPUT,DONT_CARE,DISPULL_UP},
+
+};*/
+
+
---
+++ DIO_CFG.h
@@ -0,0 +1,13 @@
+/*
+ * DIO_CFG.h
+ *
+ *  Created on:  MONDAY_03/02/2020_8:16 AM
+ *      Author: ABBAS SALAH HATATA
+ */
+
+#ifndef DIO_DRIVER_ABBAS_DIO_CFG_H
+#define DIO_DRIVER_ABBAS_DIO_CFG_H
+
+
+
+#endif /* DIO_DRIVER_ABBAS_DIO_CFG_H */
